4264e100ee40cefbffd6fccac120c2b8b8d7daef
Young <670205409@qq.com>
2018-10-23 06:39:38 +0000

    Topic order

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -13,6 +13,31 @@ class Topic extends Model
 
     public function user()
     {
-        return  $this->belongsTo(User::class);
+        return $this->belongsTo(User::class);
+    }
+
+    public function scopeWithOrder($query, $order)
+    {
+        // 不同的排序，使用不同的数据读取逻辑
+        switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
+            default:
+                $query->recentReplied();
+                break;
+        }
+
+        return $query->with('user', 'category');
+    }
+
+    public function scopeRecentReplied($query)
+    {
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    public function scopeRecent($query)
+    {
+        return $query->orderBy('created_at', 'desc');
     }
 }
