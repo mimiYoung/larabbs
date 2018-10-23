e64eb75367d37df4f4464c4de62c968815e42f4e
Young <670205409@qq.com>
2018-10-23 08:23:03 +0000

    List topics created by user

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -47,12 +47,23 @@
 
                     <ul class="dropdown-menu" role="menu">
                         <li>
-                            <a href="{{ route('users.edit', Auth::id()) }}">用户资料</a>
+                            <a href="{{ route('users.show', Auth::id()) }}">
+                                <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                                个人中心
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="{{ route('users.edit', Auth::id()) }}">
+                                <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
+                                编辑资料
+                            </a>
                         </li>
 
                         <li>
                             <a href="{{ route('logout') }}"
                                 onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                                <span class="glyphicon glyphicon-log-out" aria-hidden="true" style="color:red"></span>
                                 退出登录
                             </a>
 
