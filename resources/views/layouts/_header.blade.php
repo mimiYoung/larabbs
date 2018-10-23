9e238abcb19bc3283241248c8e49434096b663ba
Young <670205409@qq.com>
2018-10-23 06:03:21 +0000

    Category topic list

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -15,7 +15,13 @@
 
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- 左侧边栏 Navbar -->
-            <ul class="nav navbar-nav navbar-left"></ul>
+            <ul class="nav navbar-nav navbar-left">
+                <li class="{{ active_class(if_route('topics.index')) }}"><a href="{{ route('topics.index') }}">话题</a></li>
+                <li class="{{ active_class((if_route('categories.show') && if_route_param('category', 1))) }}"><a href="{{ route('categories.show', 1) }}">分享</a></li>
+                <li class="{{ active_class((if_route('categories.show') && if_route_param('category', 2))) }}"><a href="{{ route('categories.show', 2) }}">教程</a></li>
+                <li class="{{ active_class((if_route('categories.show') && if_route_param('category', 3))) }}"><a href="{{ route('categories.show', 3) }}">问答</a></li>
+                <li class="{{ active_class((if_route('categories.show') && if_route_param('category', 4))) }}"><a href="{{ route('categories.show', 4) }}">公告</a></li>
+            </ul>
 
             <!-- 右侧边栏 Navbar -->
             <ul class="nav navbar-nav navbar-right">
